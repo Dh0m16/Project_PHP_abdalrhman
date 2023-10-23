d70fe0cf0bca235b2d36d25fa2de260ba2dd0301
Dh0m16 <144840435+Dh0m16@users.noreply.github.com>
2023-10-24 00:01:58 +0300

    Update array.php

--- array.php
+++ array.php
@@ -1,15 +1,40 @@
+<form action="array.php" method="GET">
+    <input type="text" name="name">
+    <input type="submit" >
+
+</form>
+
+
+
 <?php
+$xname = $_GET["name"];
 
-$family = array("father" , "mother" , "brother" , "Sister");
+$family = [
+    "Designer" => "abdalrhman",
+    "Programmer" => "AHmad",
+    "Gamer" => "Majed",
+];
+
+$score = [
+    "abdalrhman" => "99","A+"] 
+    "AHmad" => ["80","B"] 
+    "Majed" => ["60,"D"];
 
 
-$family[1] = "love";
-$family[3] = "Sister";
+echo $score[$xname];
 
+echo "Score: " . $score[$name][0];
+echo "<br>";
+echo "Grade: " . $score[$name][1];
+
+
+
+
+//$family = array("father" , "mother" , "brother" , "Sister");
+//$family[1] = "love";
+//$family[3] = "Sister";
 //echo $family[1];
 //echo $family[3];
-
-echo count($family);
-
+//echo count($family);
 
 ?>
