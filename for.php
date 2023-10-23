0229ad7fe88f95f264ddbf7c2e5bbbe393ede45a
Dh0m16 <144840435+Dh0m16@users.noreply.github.com>
2023-10-24 00:31:46 +0300

    Update for.php

---
+++ for.php
@@ -0,0 +1,15 @@
+<?php
+// الحلقات التكرارية
+
+$family = array("Abdalrhman" , "Majed" , "Anas");
+//echo count($family);
+
+
+for($i =0 ; $i <= 5 ; $i++){
+   // echo $i + 10 . "<br>";
+   // echo "$family[$i] <br>";
+}
+
+
+
+?>
